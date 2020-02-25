0e6a85da474c9a34cf508091fb462b5e454e1a65
patrick1238 <patrick.wurzel@hotmail.de>
2020-02-25 15:03:35 +0100

    Update Config.java

--- trunk/CasePrinter/DTMS_Case_printer_1.0/src/Background_handler/Config.java
+++ trunk/CasePrinter/DTMS_Case_printer_1.0/src/Background_handler/Config.java
@@ -28,14 +28,14 @@ public class Config {
 
     HashMap<String, String> configHandler;
     ObservableList<String> configObserver;
-    private String configPath;
-    //private String configPath = "C:\\Users\\mlhpa\\Documents\\develop\\DTMS\\trunk\\CasePrinter\\DTMS_Case_printer_1.0\\Resources\\config\\config.txt";
+    //private String configPath;
+    private String configPath = "C:\\Users\\mlhpa\\Documents\\develop\\DTMS\\trunk\\CasePrinter\\DTMS_Case_printer_1.0\\Resources\\config\\config.txt";
     
     
     private Config() {
         BufferedReader br = null;
         try {
-            configPath = this.getClass().getClassLoader().getResource("config/config.txt").getFile();
+            //configPath = this.getClass().getClassLoader().getResource("config/config.txt").getFile();
             this.configHandler = new HashMap<>();
             configObserver = FXCollections.observableArrayList();
             String[] splitted;
