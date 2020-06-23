f6cfeadc6bb9477ca29a31fdbaa6120a316f1053
henneck <henneck@googlemail.com>
2020-06-23 17:06:40 +0200

    MetadataPool / method for persist metadata for a list of services added (required to reduce http requests when persisting a case -> + all child entities); endppint still not created (client/server)

---
+++ trunk/CaseManager/2.0beta/server/webapp_client/src/main/java/net/rehkind_mag/entities/filter/ClientObjectLocalChangesFilter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.rehkind_mag.entities.filter;
+
+import net.rehkind_mag.entities.ClientObjectBase;
+import net.rehkind_mag.interfaces.client.ClientObjectList;
+import net.rehkind_mag.interfaces.client.IClientObjectFilter;
+import net.rehkind_mag.interfaces.client.ReadOnlyClientObjectList;
+
+/**
+ *
+ * @author rehkind
+ */
+public class ClientObjectLocalChangesFilter<T extends ClientObjectBase> implements IClientObjectFilter<T>{
+    @Override
+    public ReadOnlyClientObjectList<T> filterClientObjectList(ReadOnlyClientObjectList<T> originalList) {
+        ClientObjectList<T> filteredList= new ClientObjectList<>();
+        for(T clientObject : originalList.getAll()){
+            if(isClientObjectInScope(clientObject)){
+                filteredList.add(clientObject);
+            }
+        }
+        return filteredList;
+    } 
+
+    @Override
+    public boolean isClientObjectInScope(T clientObject) {
+        return clientObject.hasLocalChanges();
+    }
+}
