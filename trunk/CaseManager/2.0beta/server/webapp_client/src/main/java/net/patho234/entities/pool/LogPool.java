d9a5e1ffa1f85bf359b7108723827541d63c5f84
rehkind <henneck@googlemail.com>
2020-07-06 11:58:28 +0200

    LogPool add

--- trunk/CaseManager/2.0beta/server/webapp_client/src/main/java/net/patho234/entities/pool/LogPool.java
+++ trunk/CaseManager/2.0beta/server/webapp_client/src/main/java/net/patho234/entities/pool/LogPool.java
@@ -5,10 +5,45 @@
  */
 package net.patho234.entities.pool;
 
+import java.util.concurrent.TimeoutException;
+import net.patho234.entities.ClientLogEntry;
+import net.patho234.interfaces.IHttpResponse;
+import net.patho234.interfaces.client.ReadOnlyClientObjectList;
+
 /**
  *
  * @author rehkind
  */
-public class LogPool  extends AClientObjectPool<ClientLogMessage> {
+public class LogPool  extends AClientObjectPool<ClientLogEntry> {
+
+    @Override
+    public ClientLogEntry getEntity(int ID, Boolean updatePool) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ReadOnlyClientObjectList<ClientLogEntry> getAllEntities(Boolean updatePool) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int createEntity(ClientLogEntry toCreate) throws TimeoutException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int deleteEntity(ClientLogEntry entity) throws TimeoutException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int persistEntity(ClientLogEntry entity, boolean forcePersist) throws TimeoutException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void receiveHttpResponse(IHttpResponse response) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
