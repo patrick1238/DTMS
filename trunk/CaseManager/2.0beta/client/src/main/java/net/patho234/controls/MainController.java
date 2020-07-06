fd253c8e213b52d801449f45e5e5bdf446d59cb7
patrick1238 <patrick.wurzel@hotmail.de>
2020-07-06 17:42:47 +0200

    MainView adapted and redesigned

---
+++ trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/controls/MainController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.patho234.controls;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+
+/**
+ * FXML Controller class
+ *
+ * @author patri
+ */
+public class MainController implements Initializable {
+
+    @FXML
+    private HBox mainViewHBox;
+    @FXML
+    private TableView<?> caseViewTable;
+    @FXML
+    private MenuItem addCaseMenuItem;
+    @FXML
+    private MenuItem closeMenuItem;
+    @FXML
+    private MenuItem preferencesMenuItem;
+    @FXML
+    private MenuItem profileMenuItem;
+    @FXML
+    private MenuItem aboutMenuItem;
+    @FXML
+    private TextField quickSearchField;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
