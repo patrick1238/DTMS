1c36002f975779b66eb3787b7bc802ba7454db69
rehkind <henneck@googlemail.com>
2020-07-06 18:00:08 +0200

    gui login: check login/password fields are filled in; check against SubmitterPool

--- trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/controls/LoginController.java
+++ trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/controls/LoginController.java
@@ -6,6 +6,7 @@ package net.patho234.controls;
  * and open the template in the editor.
  */
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -17,6 +18,11 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import net.patho234.entities.ClientSubmitter;
+import net.patho234.entities.pool.SubmitterPool;
+import net.patho234.gui.ClientPopup;
+import net.patho234.interfaces.client.ReadOnlyClientObjectList;
+import org.jboss.logging.Logger;
 
 /**
  * FXML Controller class
@@ -49,6 +55,53 @@ public class LoginController implements Initializable {
 
     @FXML
     private void loginPressed(ActionEvent event) {
+        String user = usernameField.getText();
+        String pwd = passwordField.getText();
+        System.out.println("PIMPANELLA");
+        boolean allowLogin=true;
+        String errorMsg="";
+        if( "".equals(user) || user == null ){
+            allowLogin=false;
+            errorMsg="Required login field is empty.\n";
+        }
+        
+        if( "".equals(pwd) || pwd == null ){
+            allowLogin=false;
+            errorMsg+="Required password field is empty.\n";
+        }
+        
+        if( !allowLogin ){
+            try {
+                new ClientPopup("Login failed", errorMsg).show( usernameField.getScene().getWindow() );
+            }catch(IOException ioEx){
+                Logger.getLogger(getClass()).error("FXML template for ClientPopup window not found...something is wrong with the JAR.");
+            }
+            return;
+        }
+        
+        // TODO:
+        // start MainWindow here
     }
     
+    private boolean isValidLogin(String login, String password){
+        ReadOnlyClientObjectList<ClientSubmitter> submitters = SubmitterPool.createPool().getAllEntities();
+        boolean isValid=false;
+        boolean loginExists=false;
+        for( ClientSubmitter cs : submitters ){
+            if(cs.getLogin().equals(login)){
+                
+                isValid = isValid || cs.getPassword().equals(password);
+            }
+        }
+        
+        if( !isValid ){
+            String errorMsg = ( !loginExists )?"Account with login '"+login+"' was not found, check for possible typos or register an account first.":"Password was incorrect.";
+            try {
+                new ClientPopup("Login failed", errorMsg).show(usernameField.getScene().getWindow());
+            }catch(IOException ioEx){
+                Logger.getLogger(getClass()).error("FXML template for ClientPopup window not found...something is wrong with the JAR.");
+            }
+        }
+        return isValid;
+    }
 }
