2227c1f180e15ca05fd77f8a4d7b0ae0297c3d2f
patrick1238 <patrick.wurzel@hotmail.de>
2020-07-07 11:16:23 +0200

    Entity table view added

--- trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/controls/elements/entityTableController.java
+++ trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/controls/elements/entityTableController.java
@@ -3,47 +3,35 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package net.patho234.controls;
+package net.patho234.controls.elements;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TabPane;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.HBox;
 
 /**
- * FXML Controller class
  *
  * @author patri
  */
-public class MainController implements Initializable {
+public class entityTableController implements Initializable  {
 
     @FXML
-    private MenuItem addCaseMenuItem;
+    private TextField fastSearch;
     @FXML
-    private MenuItem closeMenuItem;
+    private Label infoLabel;
     @FXML
-    private MenuItem preferencesMenuItem;
-    @FXML
-    private MenuItem profileMenuItem;
-    @FXML
-    private MenuItem aboutMenuItem;
-    @FXML
-    private TabPane mainTab;
-    @FXML
-    private Label loginLabel;
-
+    private TableView<?> entityLabel;
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
     
 }
