239839e8b55add904b0fb5be84abec10077de684
rehkind <henneck@googlemail.com>
2020-07-07 15:13:05 +0200

    SubmitterReceiver interface added

---
+++ trunk/CaseManager/2.0beta/lib/src/main/java/net/patho234/interfaces/client/ISubmitterReceiver.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.patho234.interfaces.client;
+
+import net.patho234.interfaces.ISubmitter;
+
+/**
+ *
+ * @author rehkind
+ */
+public interface ISubmitterReceiver {
+    public void setSubmitter(ISubmitter submitter);
+}
