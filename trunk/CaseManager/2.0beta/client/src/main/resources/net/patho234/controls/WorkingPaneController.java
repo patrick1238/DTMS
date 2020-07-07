85004ad6315e67353f1f0aaa313380333b2bab5f
patrick1238 <patrick.wurzel@hotmail.de>
2020-07-07 17:40:52 +0200

    Working and Filter controller refreshed

---
+++ trunk/CaseManager/2.0beta/client/src/main/resources/net/patho234/controls/WorkingPaneController.java
@@ -0,0 +1,120 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.patho234.controls;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author patri
+ */
+public class WorkingPaneController implements Initializable {
+
+    @FXML
+    private Label statusLabel;
+    @FXML
+    private AnchorPane workingPaneOptions;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void inspectWindowSelected(ActionEvent event) {
+    }
+
+    @FXML
+    private void searchWindowSelected(ActionEvent event) {
+    }
+
+    @FXML
+    private void exportWindowSelected(ActionEvent event) {
+    }
+
+    @FXML
+    private void caseAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void caseEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void caseDelProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void twoDimAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void twoDimEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void twoDimDelProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void threeDimAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void threeDimEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void threeDimDelProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void fourDimAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void fourDimEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void fourDimDelProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void genAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void genEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void genDelProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void methAddProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void methEditProp(ActionEvent event) {
+    }
+
+    @FXML
+    private void methDelProp(ActionEvent event) {
+    }
+    
+}
