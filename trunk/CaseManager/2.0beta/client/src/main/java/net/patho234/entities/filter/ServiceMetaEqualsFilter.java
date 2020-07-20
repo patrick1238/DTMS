4f80eaa06edebf865bc0c2b121736e661a0f993d
rehkind <henneck@googlemail.com>
2020-07-20 17:08:33 +0200

    ServiceMetaEqualsFilter isInScope method implemented

--- trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/entities/filter/ServiceMetaEqualsFilter.java
+++ trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/entities/filter/ServiceMetaEqualsFilter.java
@@ -5,10 +5,38 @@
  */
 package net.patho234.entities.filter;
 
+import java.util.Objects;
+import net.patho234.entities.ClientService;
+import net.patho234.interfaces.IMetadata;
+import org.jboss.logging.Logger;
+
 /**
  *
  * @author rehkind
  */
-public class ServiceMetaEqualsFilter {
+public class ServiceMetaEqualsFilter extends ClientObjectFilterBase<ClientService> {
+    String metadataKey;
+    Object filterObject;
+    public ServiceMetaEqualsFilter(String metadataKey, Object filterObject){
+        this.metadataKey = metadataKey;
+        this.filterObject = filterObject;
+    }
+    
+    public void setFilterObject( Object newFilterObject ){ this.filterObject = newFilterObject; }
+    
+    @Override
+    public boolean isClientObjectInScope(ClientService clientObject) {
+        if( this.filterObject == null){ return true; } // no required object set: accept all
+        
+        for (IMetadata md : clientObject.getMetadata() ){
+            if ( md.getName().equals( metadataKey ) ){
+                return (Objects.equals(filterObject, md.getData()));
+            }
+        }
+        
+        Logger.getLogger(getClass()).warn("Metadata key '"+metadataKey+"' wasn't found for ClientService. Filter potenitially applied to wrong service list?!");
+        Logger.getLogger(getClass()).warn("Service was: "+clientObject.toString());
+        return false;
+    }
     
 }
