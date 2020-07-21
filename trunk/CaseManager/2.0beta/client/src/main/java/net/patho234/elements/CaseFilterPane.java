a7f1e8a362ddf53ed98335de0f3a4e232571afe7
rehkind <henneck@googlemail.com>
2020-07-21 13:00:16 +0200

    CaseFilterPane create -> globale search options for cases

---
+++ trunk/CaseManager/2.0beta/client/src/main/java/net/patho234/elements/CaseFilterPane.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.patho234.elements;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ *
+ * @author rehkind
+ */
+public class CaseFilterPane extends AnchorPane {
+    public CaseFilterPane(){
+        super();
+        
+        FXMLLoader loader = new FXMLLoader( getClass().getResource("") );
+    }
+}
