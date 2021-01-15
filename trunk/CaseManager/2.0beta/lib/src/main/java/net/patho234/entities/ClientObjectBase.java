7653236e1f676ad392f7ce993c098f457f1889ec
rehkind <henneck@googlemail.com>
2021-01-15 17:05:30 +0100

    Date format converter moved to client

--- trunk/CaseManager/2.0beta/lib/src/main/java/net/patho234/entities/ClientObjectBase.java
+++ trunk/CaseManager/2.0beta/lib/src/main/java/net/patho234/entities/ClientObjectBase.java
@@ -20,8 +20,6 @@ import net.patho234.property.JsonObjectProperty;
  * @author rehkind
  */
 public abstract class ClientObjectBase<T extends IClientObject> implements IClientObject<T>{
-    static final protected SimpleDateFormat DATE_TIME_FORMATTER = new SimpleDateFormat("dd.MM.yyyy HH:mm:ssZ");
-    static final protected SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("dd.MM.yyyy");
     
     IntegerProperty ID = new SimpleIntegerProperty();
     JsonObjectProperty  original = new JsonObjectProperty();
